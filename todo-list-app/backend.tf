da7dd03884f7644ebfdc4bafc1aeba74d2ed9b2f
Amith Karunarathna <amith@Amith>
2023-11-17 08:31:55 +0530

    first commit

---
+++ todo-list-app/backend.tf
@@ -0,0 +1,18 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "terraform-backend"
+#     key    = "backend/Todo-app-eks.tfstate"
+#     region = "us-east-1"
+#     dynamodb_table = "dynamoDB-state-locking"
+#   }
+# }
+
+
+terraform {
+  backend "remote" {
+    organization = "nemozon-sandbox"
+    workspaces {
+      name = "K8-ArgoCD"
+    }
+  }
+}
