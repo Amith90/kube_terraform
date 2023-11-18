3a904eeb4552749891ed8aeaed217b869f04acd9
Amith90 <amithkarunaratne@gmail.com>
2023-11-18 06:58:05 +0530

    changed folder structure

--- modules/IAM/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "EKS_CLUSTER_ROLE_ARN" {
-  value = aws_iam_role.eks_cluster_role.arn
-}
-
-output "NODE_GROUP_ROLE_ARN" {
-  value = aws_iam_role.nodes_general.arn
-}
--- todo-list-app/main.tf
+++ todo-list-app/main.tf
@@ -1,7 +1,7 @@
 
 # creating VPC
 module "VPC" {
-  source           = "../modules/vpc"
+  source           = "./modules/vpc"
   REGION           = var.REGION
   PROJECT_NAME     = var.PROJECT_NAME
   VPC_CIDR         = var.VPC_CIDR
@@ -13,7 +13,7 @@ module "VPC" {
 
 # cretea NAT-NAT-GW
 module "NAT-GW" {
-  source = "../modules/nat-gw"
+  source = "./modules/nat-gw"
 
   PUB_SUB_1_A_ID = module.VPC.PUB_SUB_1_A_ID
   IGW_ID         = module.VPC.IGW_ID
@@ -25,12 +25,12 @@ module "NAT-GW" {
 
 
 module "IAM" {
-  source = "../modules/IAM"
+  source = "./modules/IAM"
   PROJECT_NAME = var.PROJECT_NAME
 }
 
 module "EKS" {
-  source               = "../modules/EKS"
+  source               = "./modules/EKS"
   PROJECT_NAME         = var.PROJECT_NAME
   EKS_CLUSTER_ROLE_ARN = module.IAM.EKS_CLUSTER_ROLE_ARN
   PUB_SUB_1_A_ID       = module.VPC.PUB_SUB_1_A_ID
@@ -41,7 +41,7 @@ module "EKS" {
 
 
 module "NODE_GROUP" {
-  source           = "../modules/Node-group"
+  source           = "./modules/Node-group"
   EKS_CLUSTER_NAME = module.EKS.EKS_CLUSTER_NAME
   NODE_GROUP_ARN   = module.IAM.NODE_GROUP_ROLE_ARN
   PRI_SUB_3_A_ID   = module.VPC.PRI_SUB_3_A_ID
